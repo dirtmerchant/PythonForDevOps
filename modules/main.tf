af1fbfc7d47d6839ad764c13f119d3359f79712c
dirtmerchant <bert@bertbullough.com>
2020-02-02 17:06:31 -0700

    adds main.tf && variables.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,22 +1,8 @@
-resource "aws_s3_bucket" "www" {
- bucket = "www.${var.domain_name}" 
- acl = "public-read"
- policy = <<POLICY
-{
-    "Version":"2012-10-17"
-    "Statement":[
-        {
-            "Sid":"AddPerm",
-            "Effect":"Allow",
-            "Principal": "*",
-            "Action":["s3:GetObject"],
-            "Resource":["arn:aws:s3:::www.${var.domain_name}/*"]
-        }
-    ]
+provider "aws" { 
+    region = "${var.aws_region}"
 }
-POLICY
 
-    website {
-        index_document = "index.html"
-    }
+module "s3" {
+    source = "./modules/s3"
+    domain_name = "${var.domain_name}"
 }
--- modules/variables.tf
+++ modules/variables.tf
@@ -1,1 +1,7 @@
-variable "domain_name" {}
+variable "aws_region" {
+    default = "us-east-1"
+}
+
+variable "domain_name" {
+    default = "devops4all.dev"
+}
