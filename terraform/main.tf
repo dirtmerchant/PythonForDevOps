e3b3eae7b54cd922b096abf4d4954240ead24a1a
dirtmerchant <bert@bertbullough.com>
2020-02-03 02:41:27 -0700

    adds cloudfront distribution

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,10 @@ module "acm" {
   domain_name = "${var.domain_name}"
 
 }
+
+module "cloudfront" {
+  source                  = "./modules/cloudfront"
+  domain_name             = "${var.domain_name}"
+  s3_www_website_endpoint = "${module.s3.s3_www_website_endpoint}"
+  acm_certificate_arn     = "${module.acm.certificate_arn}"
+}
