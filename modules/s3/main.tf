81bd5dfae4f65998249f50bdcef846b329076988
dirtmerchant <bert@bertbullough.com>
2020-02-03 01:37:26 -0700

    fixes json main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -3,16 +3,16 @@ resource "aws_s3_bucket" "www" {
   acl    = "public-read"
   policy = <<POLICY
 {
-    "Version":"2012-10-17"
-    "Statement":[
-        {
-            "Sid":"AddPerm",
-            "Effect":"Allow",
-            "Principal": "*",
-            "Action":["s3:GetObject"],
-            "Resource":["arn:aws:s3:::www.${var.domain_name}/*"]
-        }
-    ]
+"Version":"2012-10-17",
+"Statement":[
+{
+"Sid":"AddPerm",
+"Effect":"Allow",
+"Principal": "*",
+"Action":["s3:GetObject"],
+"Resource":["arn:aws:s3:::www.${var.domain_name}/*"]
+}
+]
 }
 POLICY
 
@@ -20,3 +20,4 @@ POLICY
     index_document = "index.html"
   }
 }
+
